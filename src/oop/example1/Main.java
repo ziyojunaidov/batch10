4a649d721b975167cb14ad5a386f03888b7a1197
ziyo <ziyoviddinjunaydov@gmail.com>
2023-12-08 20:05:20 -0500

    My first commit

---
+++ src/oop/example1/Main.java
@@ -0,0 +1,12 @@
+package oop.example1;
+
+public class Main {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.name = "Mercedes";
+        c1.speed = 150;
+        c1.engineVolume = 2.8;
+
+        System.out.println(c1.name + " is traveling " + c1.speed + " km/h " + " and it has " + c1.engineVolume + " liter engine");
+    }
+}
